763c51acc4cb706ca91575eb322111f88bc1eecd
Seb Insua <me@sebinsua.com>
2017-09-13 19:54:42 +0100

    feat: empty example app

---
+++ index.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class ClickOutside extends Component {
+  static propTypes = {
+    onClickOutside: PropTypes.func.isRequired
+  }
+
+  getContainer = ref => {
+    this.container = ref
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.handle, true)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handle, true)
+  }
+
+  handle = e => {
+    const { onClickOutside } = this.props
+    const el = this.container
+    if (!el.contains(e.target)) onClickOutside(e)
+  }
+
+  render() {
+    const { children, onClickOutside, ...props } = this.props
+    return (
+      <div {...props} ref={this.getContainer}>
+        {children}
+      </div>
+    )
+  }
+}
+
+export default ClickOutside
