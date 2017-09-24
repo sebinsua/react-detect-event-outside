b37f46c4576d9eb17abb6a26a0fc2ec9b16ea6df
Seb Insua <me@sebinsua.com>
2017-09-24 23:40:11 +0100

    fix: flow definition for wrapDisplayName

--- src/wrapDisplayName.js
+++ src/wrapDisplayName.js
@@ -1,16 +1,9 @@
+// @flow
+
 import type { ComponentType } from 'react'
 
-const getDisplayName = (Component: ComponentType<*>): string => {
-  if (typeof Component === 'string') {
-    return Component
-  }
-
-  if (!Component) {
-    return undefined
-  }
-
-  return Component.displayName || Component.name || 'Component'
-}
+const getDisplayName = (Component: ComponentType<*>): string =>
+  Component.displayName || Component.name || 'Component'
 
 const wrapDisplayName = (
   BaseComponent: ComponentType<*>,
